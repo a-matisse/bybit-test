d2c5deea612bf8a19b863ebf219d19a441c0ca2c
SeeMemes <provotorov-02@mail.ru>
2024-08-09 19:49:59 +0300

    fix

--- src/main/java/org/seememes/bybit/tester/entity/SecondTestEntity.java
+++ src/main/java/org/seememes/bybit/tester/entity/SecondTestEntity.java
@@ -1,10 +1,8 @@
 package org.seememes.bybit.tester.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class SecondTestEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/org/seememes/bybit/tester/repository/SecondTestEntityRepository.java
+++ src/main/java/org/seememes/bybit/tester/repository/SecondTestEntityRepository.java
@@ -2,6 +2,8 @@ package org.seememes.bybit.tester.repository;
 
 import org.seememes.bybit.tester.entity.SecondTestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SecondTestEntityRepository extends JpaRepository<SecondTestEntity, Long> {
 }
